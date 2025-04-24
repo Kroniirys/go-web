81ff5059493a4a1efab0c522838e415e6ec015a6
Nguyễn ĐạtĐạt <dat01687241584@gmail.com>
2025-04-24 20:34:11 +0700

    Add MySQL configuration and database schema

--- config/database.go
+++ config/database.go
@@ -3,24 +3,44 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	dsn := "root:01680865@tcp(127.0.0.1:3306)/go_web"
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get database credentials from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Create connection string
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+
+	// Open database connection
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
+	// Test the connection
 	err = DB.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("Kết nối database thành công!")
+	fmt.Println("Connected to MySQL database!")
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module go-web
 
-go 1.21.0
+go 1.21
 
 toolchain go1.22.3
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.17.0
 )
